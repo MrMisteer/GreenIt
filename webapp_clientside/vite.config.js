295f0bbe48c30ed581b42bbc640cc30b89feecea
MrMisteer <129770157+MrMisteer@users.noreply.github.com>
2025-04-27 20:42:05 +0200

    deploiement v3

--- webapp_clientside/vite.config.js
+++ webapp_clientside/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  root: './webapp_clientside', // Set the correct root directory
+  // Supprimez la ligne root si elle existe encore
   plugins: [vue()],
+  build: {
+    outDir: 'dist'
+  }
 })
